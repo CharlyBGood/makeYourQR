2e185e0047cf18e6a7a0fe8a6e341d6654400ad2
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-06-24 03:49:27 -0300

    create qr instance

---
+++ main.js
@@ -0,0 +1,13 @@
+const containerQr = document.getElementById('container_qr');
+const form = document.getElementById('form');
+const link = document.getElementById('url');
+
+
+// new QRCode(containerQr, 'docs.google.com/forms/d/e/1FAIpQLSdU0ZnjL4NlnZEdeEnrPYxDhE2WJF6O9MwHP9IyioAhVAGY9g/viewform');
+
+const QR = new QRCode(containerQr);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    QR.makeCode(link.value);
+})
